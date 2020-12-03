64244d99280624dd69bb62db7f2bd77abbf8caf2
yufei.liu <yufei.liu@wolaidai.com>
2020-12-03 15:41:25 +0800

    2020年12月03日15:41:17，递归创建单链表

--- src/main/java/com/fish/algorithm/LinkedListTest.java
+++ src/main/java/com/fish/algorithm/LinkedListTest.java
@@ -14,28 +14,44 @@ import lombok.Data;
 public class LinkedListTest {
 
     public static void main(String[] args) {
-        // singlyLinkedListReverse(createNewSinglyLinkedList());
-        singlyLinkedListReverseRecursively(createNewSinglyLinkedList());
+        printLinkedList(createNodeList());
+        printLinkedList(reverseNodeList(createNodeList()));
+        printLinkedList(reverseNodeList2(createNodeList()));
+
+        printLinkedList(createNodeList2(5));
+        printLinkedList(reverseNodeList(createNodeList2(5)));
+        printLinkedList(reverseNodeList2(createNodeList2(5)));
     }
 
     /**
      * 创建单链表
      */
-    public static Node<Integer> createNewSinglyLinkedList() {
+    public static Node<Integer> createNodeList() {
         Node<Integer> current = new Node<>();
         Node<Integer> head = current;
         for (int i = 1; i <= 5; i++) {
             current.next = new Node<>(i);
             current = current.next;
         }
-        printLinkedList(head.next);
         return head.next;
+    }
+
+    /**
+     * 创建单链表 - 递归
+     */
+    public static Node<Integer> createNodeList2(int i) {
+        if (i <= 0) {
+            return null;
+        }
+        Node<Integer> node = new Node<>(i);
+        node.next = createNodeList2(--i);
+        return node;
     }
 
     /**
      * 单链表反转
      */
-    public static void singlyLinkedListReverse(Node<Integer> head) {
+    public static Node<Integer> reverseNodeList(Node<Integer> head) {
         Node<Integer> pre = null;
         Node<Integer> cur = head;
         while (cur != null) {
@@ -44,22 +60,17 @@ public class LinkedListTest {
             pre = cur;
             cur = temp;
         }
-        assert pre != null;
-        printLinkedList(pre);
+        return pre;
     }
 
     /**
      * 单链表反转 - 递归
      */
-    public static void singlyLinkedListReverseRecursively(Node<Integer> head) {
-        printLinkedList(doSinglyLinkedListReverseRecursively(head));
-    }
-
-    public static Node<Integer> doSinglyLinkedListReverseRecursively(Node<Integer> head) {
+    public static Node<Integer> reverseNodeList2(Node<Integer> head) {
         if (head == null || head.next == null) {
             return head;
         }
-        Node<Integer> newHead = doSinglyLinkedListReverseRecursively(head.next);
+        Node<Integer> newHead = reverseNodeList2(head.next);
         head.next.next = head;
         head.next = null;
         return newHead;
@@ -81,7 +92,8 @@ public class LinkedListTest {
         public T item;
         public Node<T> next;
 
-        public Node() {}
+        public Node() {
+        }
 
         public Node(T item) {
             this.item = item;
