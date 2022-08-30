76c0f0e1ee651a3a244e9e517b40921676a5ec5b
yufei.liu <yufei.liu@welab-inc.com>
2022-08-30 22:10:28 +0800

    2022年08月30日22:10:12，removeElement

--- src/main/java/com/fish/leetcode/P27_RemoveElement.java
+++ src/main/java/com/fish/leetcode/P27_RemoveElement.java
@@ -9,10 +9,10 @@ package com.fish.leetcode;
 public class P27_RemoveElement {
 
     public static void main(String[] args) {
-        int[] nums = {0,1,2,2,2,2,3,0,4,2};
-        // int[] nums = {2,2,2,2,3,0,4,2};
+        //int[] nums = {0,1,2,2,2,2,3,0,4,2};
+         int[] nums = {2,2,2,2,3,0,4,2};
         int val = 2;
-        System.err.println(removeElement(nums, val));
+        System.err.println(removeElement2(nums, val));
     }
 
     public static int removeElement(int[] nums, int val) {
@@ -26,6 +26,18 @@ public class P27_RemoveElement {
                 i--;
             }
         }
+        return i;
+    }
+
+    public static int removeElement2(int[] nums, int val) {
+        int i = 0;
+
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i++] = nums[j];
+            }
+        }
+
         return i;
     }
 }
