bc52e65fc876f0c1f3ac1d2f516bac1dfc106524
yufei.liu <coderunner@foxmail.com>
2022-08-30 19:01:01 +0800

    2022年08月30日19:00:49，P27 移除数组中的指定元素

---
+++ src/main/java/com/fish/leetcode/P27_RemoveElement.java
@@ -0,0 +1,31 @@
+package com.fish.leetcode;
+
+/**
+ * @author yufei.liu
+ * @version v1.0
+ * @description
+ * @date 2022-08-30 17:34
+ */
+public class P27_RemoveElement {
+
+    public static void main(String[] args) {
+        int[] nums = {0,1,2,2,2,2,3,0,4,2};
+        // int[] nums = {2,2,2,2,3,0,4,2};
+        int val = 2;
+        System.err.println(removeElement(nums, val));
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[i] == val && nums[j] != val) {
+                nums[i] = nums[j];
+                nums[j] = val;
+            }
+            if (nums[i++] == val) {
+                i--;
+            }
+        }
+        return i;
+    }
+}
